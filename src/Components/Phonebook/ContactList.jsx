708444f6334ab1ee21189e70fe2c7fd83c3d7ca6
dzhafarovm <maksim8797@gmail.com>
2021-10-15 17:44:42 +0400

    add contacts sort

--- src/Components/Phonebook/ContactList.jsx
+++ src/Components/Phonebook/ContactList.jsx
@@ -2,6 +2,8 @@ import ContactListItem from './ContactItem';
 import css from './phonebook-css/ContactList.module.css';
 
 export default function ContactList({ contacts }) {
+  contacts.sort((a, b) => a.name.localeCompare(b.name));
+
   return (
     <ul className={css.list}>
       {contacts.map(({ id, name, phone }) => (
